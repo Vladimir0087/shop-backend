92cf0c7d8b1ad7482f424708710546d4f00ef3d9
Uladzimir Branisheuski <Uladzimir_Branisheuski@epam.com>
2023-03-31 12:45:20 +0300

    fix: cors

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -5,9 +5,6 @@ import { products } from "./../../mocks/data";
 export const getProductsList = async () : Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
     body: JSON.stringify(products, null, 2)
   };
 };
